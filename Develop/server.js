d0988db60e4befaf5fbe503fcaaab7970a4d9d29
franko88bit <franks188@icloud.com>
2023-06-21 21:44:30 -0400

    fixed bugs on server side code

--- Develop/server.js
+++ Develop/server.js
@@ -11,7 +11,11 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('/notes', function (req, res) {
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "/public/notes.html"));
 
 })
@@ -22,10 +26,10 @@ app.route("/api/notes")
     })
 
     .post(function (req, res) {
-        let json_F_P = path.join(__dirname, "/db/db.json");
+        let json_fp = path.join(__dirname, "/db/db.json");
         let newNote = req.body;
 
-        let biggestId == 99;
+        let biggestId = 99;
 
         for (let i = 0; i < database.length; i++) {
             let each_note = database[i];
@@ -33,19 +37,52 @@ app.route("/api/notes")
             if (each_note.id > biggestId) {
                 biggestId = each_note.id;
             }
+
         }
+        newNote.id = biggestId + 1;
+
+        database.push(newNote)
+
+        fs.writeFile(json_F_P, JSON.stringify(database), function (err) {
+
+            if (err) {
+                return console.log(err);
+
+            }
+            console.log('Your note was saved!!!');
+        });
+        res.json(newNote);
     })
 
-newNote.id = biggestId + 1;
 
-database.push(newNote)
 
-fs.writeFile(json_F_P, JSON.stringify(database), function (err) {
-    if (err) {
-        return console.log(err);
 
+app.delete("/api/notes/:id", function (req, res) {
+    let json_F_P = path.join(__dirname, "/db/db.json");
+
+    for (let i = 0; i < database.length; i++) {
+
+        if (database[i].id == req.params.id) {
+            database.splice(i, 1);
+            break;
+        }
     }
-    console.log('Your note was saved!!!');
+
+    fs.writeFileSync(json_F_P, JSON.stringify(database), function (err) {
+
+        if (err) {
+            return console.log(err);
+        } else {
+            console.log("Your note was deleted!!!");
+        }
+
+    });
+    res.json(database);
 });
-res.json(newNote);
+
+app.listen(port, function () {
+    console.log("App is listneing on PORT " + port);
+});
+
+
 
